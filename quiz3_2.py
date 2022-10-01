e1050e9a4cf0001dc2905f7da7fffc352703932f
kfokusd <93218564+kfokusd@users.noreply.github.com>
2022-10-01 16:48:54 -0700

    Add files via upload

---
+++ quiz3_2.py
@@ -0,0 +1,26 @@
+lLOrig = [10, 11, 15, 21, 38, 43, 90]
+lROrig = [100, 101, 106, 210, 880, 4300, 5010]
+lNOrig = [100, -101, -106, 3210, 890, 6300, -10]
+##########
+# do NOT change the code above when submitting
+##########
+
+
+##########
+# Write your two functions here
+
+
+##########
+
+
+##########
+# do NOT change the code below when submitting
+##########
+print(computeTotalDelta(lLOrig))
+print(computeTotalDelta(lROrig))
+print(computeTotalDelta(lNOrig))
+
+# Expected Output:
+# 83
+# 4916
+# 17568
